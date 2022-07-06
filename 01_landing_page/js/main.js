64d5202302988707655249cf3a802c5155635409
belo4ya <41exey.kov41ev@gmail.com>
2022-07-06 04:35:00 +0300

    mobile menu

--- 01_landing_page/js/main.js
+++ 01_landing_page/js/main.js
@@ -1,19 +1,28 @@
-$(document).ready(function () {
-  const owl = $('.owl-carousel')
+const owl = $('#owl_carousel')
 
-  owl.owlCarousel({
-    center: true,
-    loop: true,
-    margin: 30,
-    startPosition: 1,
-    items: 3,
-  })
-
-  $('.slider__btn-prev').click(function () {
-    owl.trigger('prev.owl.carousel')
-  })
-
-  $('.slider__btn-next').click(function () {
-    owl.trigger('next.owl.carousel')
-  })
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
 })
+
+$('.slider__btn-prev').click(function () {
+  owl.trigger('prev.owl.carousel')
+})
+
+$('.slider__btn-next').click(function () {
+  owl.trigger('next.owl.carousel')
+})
+
+
+const navBtn = document.getElementById('nav_toggle')
+const nav = document.getElementById('nav')
+const menuIcon = document.getElementById('menu_icon')
+
+navBtn.onclick = function () {
+  document.body.classList.toggle('no-scroll')
+  nav.classList.toggle('nav-mobile')
+  menuIcon.classList.toggle('menu-icon-active')
+}
